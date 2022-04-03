0e84b19ce141d138c4ed81e56aaeea8a0d026b3f
twoody <tanner.l.woody@gmail.com>
2022-04-03 07:19:40 -0500

    styled

--- src/components/common/FlexTable.vue
+++ src/components/common/FlexTable.vue
@@ -16,4 +16,28 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.flex-table-wrapper {
+	align-items: flex-start;
+	border: 1px solid black;
+	border-radius: 4px;
+	display: flex;
+	flex-direction: row;
+	padding-left: 10px;
+	padding-right: 10px;
+	width: 100%;
+
+	.category-section {
+		border-right: 1px solid black;
+		flex-grow: 0;
+		flex-shrink: 1;
+		padding-right: 7px;
+		width: auto;
+	}
+	.content-section {
+		flex-grow: 1;
+		flex-shrink: 0;
+		text-align: center;
+		width: 100%;
+	}
+}
 </style>
