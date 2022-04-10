b519bed5f079c4c84473be1a7fbbf49cc66fe26f
twoody <tanner.l.woody@gmail.com>
2022-04-10 16:40:04 -0700

    alias src for ease

--- vue.config.js
+++ vue.config.js
@@ -14,6 +14,7 @@ module.exports = {
 				constants: path.resolve(__dirname, "src/constants"),
 				img: path.resolve(__dirname, "assets/img"),
 				services: path.resolve(__dirname, "src/services/"),
+				src: path.resolve(__dirname, "src"),
 				store: path.resolve(__dirname, "src/store/"),
 				styles: path.resolve(__dirname, "assets/styles/"),
 				svgs: path.resolve(__dirname, "src/assets/svgs/"),
