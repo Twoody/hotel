90fe5edb27448d50c1c23a8bb952fb14bc974d50
twoody <tanner.l.woody@gmail.com>
2022-04-10 17:28:26 -0700

    getting imports right

---
+++ src/components/entities/AmenitiesTable.vue
@@ -0,0 +1,64 @@
+<template>
+	<div class="amenities-section">
+		<!-- TODO: Componentize -->
+		<h2>Kitchen</h2>
+		<!-- TODO: Pull items from a database -->
+		<Accordion>
+			<template #title>
+				Blender
+			</template>
+			<template #content>
+				<FlexTable>
+					<template #category>
+						Year
+					</template>
+					<template #content>
+						2021
+					</template>
+				</FlexTable>
+				<FlexTable>
+					<template #category>
+						Brand
+					</template>
+					<template #content>
+						Vitamix
+					</template>
+				</FlexTable>
+				<FlexTable>
+					<template #category>
+						Model
+					</template>
+					<template #content>
+						Explorian E310
+					</template>
+				</FlexTable>
+
+			</template>
+		</Accordion>
+	</div>
+</template>
+
+<script>
+import {AMENITIES} from "src/constants.js"
+import Accordion from "components/common/Accordion"
+import FlexTable from "components/common/FlexTable"
+
+export default {
+	name: "AmenitiesTable",
+	components:
+	{
+		Accordion,
+		FlexTable,
+	},
+	computed:
+	{
+		/**
+		 * @returns {Array} The different amenities located and associated with the kitchen
+		 */
+		kitchen () 
+		{
+			return AMENITIES.KITCHEN
+		},
+	},
+}
+</script>
--- src/views/Amenities.vue
+++ src/views/Amenities.vue
@@ -1,56 +1,18 @@
 <template>
 	<div class="amenities-wrapper">
 		<h1>Amenities</h1>
-		<div class="amenities-section">
-			<!-- TODO: Componentize -->
-			<h2>Kitchen</h2>
-			<!-- TODO: Pull items from a database -->
-			<Accordion>
-				<template #title>
-					Blender
-				</template>
-				<template #content>
-					<FlexTable>
-						<template #category>
-							Year
-						</template>
-						<template #content>
-							2021
-						</template>
-					</FlexTable>
-					<FlexTable>
-						<template #category>
-							Brand
-						</template>
-						<template #content>
-							Vitamix
-						</template>
-					</FlexTable>
-					<FlexTable>
-						<template #category>
-							Model
-						</template>
-						<template #content>
-							Ventura 800	
-						</template>
-					</FlexTable>
-
-				</template>
-			</Accordion>
-		</div>
+		<AmenitiesTable />
 	</div>
 </template>
 
 <script>
-import Accordion from "components/common/Accordion"
-import FlexTable from "components/common/FlexTable"
+import AmenitiesTable from "components/entities/AmenitiesTable"
 
 export default {
 	name: "Amenities",
 	components:
 	{
-		Accordion,
-		FlexTable,
+		AmenitiesTable,
 	},
 	created: function()
 	{
