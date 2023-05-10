cb6567863d88bba32fb4065292e72a45f55e45aa
twoody <tanner.l.woody@gmail.com>
2023-05-10 13:02:25 -0700

    rough draft of full imports

--- src/main.js
+++ src/main.js
@@ -2,10 +2,47 @@ import { createApp } from "vue"
 import App from "./App.vue"
 
 import Accordion from "teahub"
+import AmenitiesSection from "components/accordions/AmenitiesSection.vue"
+import AvailabilitySearch from "components/forms/AvailabilitySearch.vue"
+import AvailabilitySearchBar from "components/inputs/AvailabilitySearchBar.vue"
+import BookButton from "components/buttons/submissions/BookButton.vue"
+import ChipFilters from "components/buttons/filters/ChipFilters.vue"
+import Copyright from "components/common/Copyright.vue"
+import DateSelector from "components/inputs/DateSelector.vue"
+import DialogModal from "components/modals/DialogModal.vue"
+import Filters from "components/buttons/filters/Filters.vue"
+import FlexTable from "components/common/FlexTable.vue"
+import LoadingBar from "components/common/loading/LoadingBar.vue"
+import MapCard from "components/cards/MapCard.vue"
 import MyButton from "teahub"
-import MyFilter from "teahub"
-import "teahub/dist/style.css"
+import MyDate from "components/inputs/MyDate.vue"
+import MyFilter from "components/buttons/filters/MyFilter.vue"
+import QuestionAccordion from "components/accordions/QuestionAccordion.vue"
+import SideMenu from "components/menus/SideMenu.vue"
+import Spinner from "components/common/loading/Spinner.vue"
+import Validatable from "components/common/Validatable.vue"
+app.use(Accordion)
+app.use(AmenitiesSection)
+app.use(AvailabilitySearch)
+app.use(AvailabilitySearchBar)
+app.use(BookButton)
+app.use(ChipFilters)
+app.use(Copyright)
+app.use(DateSelector)
+app.use(DialogModal)
+app.use(Filters)
+app.use(FlexTable)
+app.use(LoadingBar)
+app.use(MapCard)
+app.use(MyButton)
+app.use(MyDate)
+app.use(MyFilter)
+app.use(QuestionAccordion)
+app.use(SideMenu)
+app.use(Spinner)
+app.use(Validatable)
 
+import "teahub/dist/style.css"
 import router from "./router"
 import store from "./store/store.js"
 import { faCheck } from "@fortawesome/free-solid-svg-icons"
