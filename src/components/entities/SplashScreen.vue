d7bf025f0b78baaaacdda5e21106e18561cd2723
Tanner Woody <tanner.l.woody@gmail.com>
2025-01-14 21:03:40 -0800

    support a splash screen to prevent race conditions during initial setup

---
+++ src/components/entities/SplashScreen.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="splash-screen">
+		<h2>Loading...</h2>
+		<Spinner size="x-large" />
+	</div>
+</template>
+
+<script>
+export default {
+	name: "SplashScreen",
+}
+</script>
+
+<style scoped lang="less">
+.splash-screen {
+	align-items: center;
+	color: #000;
+	display: flex;
+	flex-direction: column;
+	font-size: 2em;
+	height: 100vh;
+	justify-content: center;
+	width: 100%;
+}
+</style>
+
