dac62400885b116bf6c405df9f2c77c6b46ae3ac
Tanner Woody <tanner.l.woody@gmail.com>
2025-01-31 11:37:59 -0800

    bunch of stuff for teahub

---
+++ src/components/common/Checkbox.vue
@@ -0,0 +1,28 @@
+Molecule for drawing a single checkbox plus label on an entire row.
+Will use the slot as the label.
+
+<template>
+	<div
+		class='checkbox'
+		@click="$emit('input', !value)"
+	>
+		<Toggle :selected='value'>
+			<slot></slot>
+		</Toggle>
+	</div>
+</template>
+
+<script>
+import Toggle from '@/components/common/Toggle.vue'
+
+export default {
+	name: 'Checkbox',
+	components: {
+		Toggle,
+	},
+	props:
+	{
+		value: Boolean,
+	},
+}
+</script>
---
+++ src/components/common/LoadingToggle.vue
@@ -0,0 +1,77 @@
+<template>
+	<div class='loading-toggle'>
+		<div class='circle'></div>
+		<LoadingBar
+			size='medium'
+			tall
+		/>
+	</div>
+</template>
+
+<script>
+export default
+{
+	name: 'LoadingToggle',
+}
+</script>
+
+<style scoped lang="less">
+
+	.loading-toggle {
+		display: flex;
+		flex-direction: row;
+		justify-content: left;
+		align-items: center;
+		margin-bottom: 20px;
+
+		.loading-bar {
+			margin: 0 0 0 15px;
+		}
+
+		.circle {
+			position: relative;
+			overflow: hidden;
+			margin-right: 0 !important;
+			height: 32px;
+			width: 32px;
+			border-radius: 50%;
+			border: 1px solid green;
+			background: rgba(0, 178, 7, .18); /* quite pale green */
+
+			/* Imported from loading.less */
+			.loading-background();
+
+			&:after {
+				opacity: .25;
+			}
+		}
+	}
+
+@-webkit-keyframes bg-fade {
+		0%{background-position-x:0%}
+		100%{background-position-x:-100%}
+}
+
+.loading-background()
+{
+	position: relative;
+	text-align: left;
+
+	&:after{
+		animation: bg-fade 1s linear infinite;
+		background: linear-gradient(to right,
+			rgba(255,255,255,.25) 0%,
+			rgba(255,255,255,.25) 1%,
+			rgba(255,255,255,0) 15%,
+			rgba(255,255,255,.25) 50%,
+			rgba(255,255,255,0) 65%,
+			rgba(255,255,255,.25) 100%);
+		background-size: 200% 200%;
+		content: "";
+		height: 100%;
+		position: absolute;
+		width: 100%;
+	}
+}
+
+</style>
