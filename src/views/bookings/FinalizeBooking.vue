c63d59e6b95b50eca681c4d3f670de90779f9ff0
Tanner Woody <tanner.l.woody@gmail.com>
2025-01-31 22:22:12 -0800

    kinda better

--- src/views/bookings/FinalizeBooking.vue
+++ src/views/bookings/FinalizeBooking.vue
@@ -20,37 +20,117 @@
 				</QuestionAccordion>
 			</section>
 
-			<Checkbox v-model="hasCats">
-				Cats
-			</Checkbox>
-			<Checkbox v-model="hasDogs">
-				Dogs
-			</Checkbox>
-			<Checkbox v-model="hasBabies">
-				Babies
-			</Checkbox>
-			<Checkbox v-model="hasToddlers">
-				Toddlers
-			</Checkbox>
-			<Checkbox v-model="hasKids">
-				Kids
-			</Checkbox>
-
 			<section class="finalize-booking">
 				<h3>Booking Details</h3>
 				<form @submit.prevent="submitBookingDetails">
-					<!-- Total Guests -->
+					<!-- Checkboxes in Two Columns -->
+					<div class="checkbox-grid">
+						<div class="checkbox-column">
+							<Checkbox v-model="hasCats">
+								Cats
+							</Checkbox>
+							<Checkbox v-model="hasDogs">
+								Dogs
+							</Checkbox>
+							<Checkbox v-model="hasBabies">
+								Babies
+							</Checkbox>
+						</div>
+						<div class="checkbox-column">
+							<Checkbox v-model="hasToddlers">
+								Toddlers
+							</Checkbox>
+							<Checkbox v-model="hasKids">
+								Kids
+							</Checkbox>
+						</div>
+					</div>
+
+					<!-- Total Aults -->
 					<label class="user-setting-input-wrapper">
-						Total Guests:
+						Total Adults:
 						<Validatable
 							class="user-setting-input"
-							:error="errors.totalGuests"
+							:error="errors.Adults"
 						>
 							<div class="input-wrapper">
 								<input
 									type="number"
-									v-model="formData.totalGuests"
+									v-model="formData.Adults"
 									required min="1"
+								>
+							</div>
+						</Validatable>
+					</label>
+
+					<!-- Pets -->
+					<label class="user-setting-input-wrapper">
+						Pets:
+						<div class="input-wrapper pets-inputs">
+							<!-- Only show the Cats number input if hasCats is true -->
+							<label v-if="hasCats">
+								<input
+									v-model="formData.cats"
+									type="number"
+									min="0"
+									max="10"
+								>
+								Cats
+							</label>
+							<!-- Only show the Dogs number input if hasDogs is true -->
+							<label v-if="hasDogs">
+								<input
+									v-model="formData.dogs"
+									type="number"
+									min="0"
+									max="10"
+								>
+								Dogs
+							</label>
+						</div>
+					</label>
+
+					<!-- Babies, Toddlers, Kids -->
+					<!-- Only show the Babies input if hasBabies is true -->
+					<label class="user-setting-input-wrapper" v-if="hasBabies">
+						Babies (0-2 yrs):
+						<Validatable class="user-setting-input">
+							<div class="input-wrapper">
+								<input
+									type="number"
+									v-model="formData.babies"
+									min="0"
+									max="10"
+								>
+							</div>
+						</Validatable>
+					</label>
+
+					<!-- Only show the Toddlers input if hasToddlers is true -->
+					<label class="user-setting-input-wrapper" v-if="hasToddlers">
+						Toddlers (2-5 yrs):
+						<Validatable class="user-setting-input">
+							<div class="input-wrapper">
+								<input
+									type="number"
+									v-model="formData.toddlers"
+									min="0"
+									max="10"
+								>
+							</div>
+						</Validatable>
+					</label>
+
+					<!-- Only show the Kids input if hasKids is true -->
+					<label class="user-setting-input-wrapper" v-if="hasKids">
+						Kids (6-12 yrs):
+						<Validatable class="user-setting-input">
+							<div class="input-wrapper">
+								<input
+									type="number"
+									v-model="formData.kids"
+									min="0"
+									max="10"
 								>
 							</div>
 						</Validatable>
@@ -65,74 +145,6 @@
 						>
 							<div class="input-wrapper">
 								<textarea v-model="formData.specialRequests"/>
-							</div>
-						</Validatable>
-					</label>
-
-					<!-- Pets -->
-					<label class="user-setting-input-wrapper">
-						Pets:
-						<div class="input-wrapper">
-							<label>
-								<input
-									v-model="formData.cats"
-									type="number"
-									min="0"
-									max="10"
-								>
-								Cats
-							</label>
-							<label>
-								<input
-									v-model="formData.dogs"
-									type="number"
-									min="0"
-									max="10"
-								>
-								Dogs
-							</label>
-						</div>
-					</label>
-
-					<!-- Babies, Toddlers, Kids -->
-					<label class="user-setting-input-wrapper">
-						Babies (0-2 yrs):
-						<Validatable class="user-setting-input">
-							<div class="input-wrapper">
-								<input
-									type="number"
-									v-model="formData.babies"
-									min="0"
-									max="10"
-								>
-							</div>
-						</Validatable>
-					</label>
-
-					<label class="user-setting-input-wrapper">
-						Toddlers (2-5 yrs):
-						<Validatable class="user-setting-input">
-							<div class="input-wrapper">
-								<input
-									type="number"
-									v-model="formData.toddlers"
-									min="0"
-									max="10"
-								>
-							</div>
-						</Validatable>
-					</label>
-
-					<label class="user-setting-input-wrapper">
-						Kids (6-12 yrs):
-						<Validatable class="user-setting-input">
-							<div class="input-wrapper">
-								<input
-									type="number"
-									v-model="formData.kids"
-									min="0"
-									max="10"
-								>
 							</div>
 						</Validatable>
 					</label>
@@ -167,8 +179,7 @@ import Checkbox from "@/components/common/Checkbox.vue"
 
 export default {
 	name: "FinalizeBooking",
-	components:
-	{
+	components: {
 		Checkbox,
 	},
 	props: {
@@ -177,8 +188,7 @@ export default {
 			type: Object,
 		},
 	},
-	data () 
-	{
+	data () {
 		return {
 			hasCats: false,
 			hasDogs: false,
@@ -186,7 +196,7 @@ export default {
 			hasToddlers: false,
 			hasKids: false,
 			formData: {
-				totalGuests: 1,
+				Adults: 1,
 				specialRequests: "",
 				cats: 0,
 				dogs: 0,
@@ -197,24 +207,17 @@ export default {
 			errors: {},
 		}
 	},
-	created () 
-	{
-		if (this.bookingID)
-		{
+	created () {
+		if (this.bookingID) {
 			this.loadSavedFormData()
 		}
 	},
-	computed:
-	{
-		bookingID ()
-		{
+	computed: {
+		bookingID () {
 			return this.booking.id || ""
 		},
-
-		cacheBookingKey ()
-		{
-			if (!this.bookingID)
-			{
+		cacheBookingKey () {
+			if (!this.bookingID) {
 				return ""
 			}
 			return `bookingFormData-${this.bookingID}`
@@ -224,46 +227,39 @@ export default {
 		/**
 		 * @returns {} - Load form data from localStorage
 		 */
-		loadSavedFormData () 
-		{
+		loadSavedFormData () {
 			const savedData = localStorage.getItem(this.cacheBookingKey)
-			if (savedData) 
-			{
+			if (savedData) {
 				this.formData = JSON.parse(savedData)
 			}
 		},
 
 		/**
-		 * @returns {} -  Save form data to localStorage
+		 * @returns {} - Save form data to localStorage
 		 */
-		saveFormData () 
-		{
+		saveFormData () {
 			localStorage.setItem(this.cacheBookingKey, JSON.stringify(this.formData))
 		},
 
 		/**
-		 * @returns {} -  Validate and submit form
+		 * @returns {} - Validate and submit form
 		 */
-		submitBookingDetails () 
-		{
+		submitBookingDetails () {
 			this.errors = {}
-			if (!this.formData.totalGuests || this.formData.totalGuests < 1) 
-			{
-				this.errors.totalGuests = "Total guests must be at least 1."
+			if (!this.formData.Adults || this.formData.Adults < 1) {
+				this.errors.Adults = "Total adults must be at least 1."
 			}
 
-			if (Object.keys(this.errors).length === 0) 
-			{
+			if (Object.keys(this.errors).length === 0) {
 				this.saveFormData()
 				alert("Booking details saved successfully!")
 			}
 		},
 
 		/**
-		 * @returns {} -  Placeholder for payment integration
+		 * @returns {} - Placeholder for payment integration
 		 */
-		onPayNow () 
-		{
+		onPayNow () {
 			console.log("User clicked Pay Now for booking:", this.bookingID)
 			alert("Redirecting to payment page... (Stripe integration pending)")
 		},
@@ -293,6 +289,14 @@ export default {
 	p {
 		font-size: 18px;
 		margin-top: 10px;
+	}
+
+	/* Grid for the checkboxes in two columns */
+	.checkbox-grid {
+		display: grid;
+		grid-template-columns: repeat(2, 1fr);
+		gap: 10px;
+		margin-bottom: 20px;
 	}
 
 	.user-setting-input-wrapper {
@@ -349,3 +353,4 @@ export default {
 	}
 }
 </style>
+
