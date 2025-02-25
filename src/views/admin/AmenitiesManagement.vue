28f083fc72d45c0ac144fdd791a5f825bad73ca7
Tanner Woody <tanner.l.woody@gmail.com>
2025-02-25 13:43:30 -0800

    provide skeletons for the different sections

---
+++ src/views/admin/AmenitiesManagement.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="amenities-management-wrapper">
+		<h1>User Management</h1>
+
+		<div class="amenities-mgmt-body">
+			<p>
+				Manage the amenities section for a hotel
+			</p>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "AmenitiesManagement",
+}
+</script>
+
+<style scoped>
+.amenities-management-wrapper {
+  text-align: center;
+  padding: 20px;
+}
+</style>
---
+++ src/views/admin/GuidesManagement.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="guides-management-wrapper">
+		<h1>User Management</h1>
+
+		<div class="guides-mgmt-body">
+			<p>
+				Manage the guides content and values
+			</p>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "GuidesManagement",
+}
+</script>
+
+<style scoped>
+.guides-management-wrapper {
+  text-align: center;
+  padding: 20px;
+}
+</style>
