278bf7d422cc5e1e46c01297e5894962da97ec33
Tanner Woody <tanner.l.woody@gmail.com>
2025-03-24 23:16:28 -0700

    line length

--- vitest.config.js
+++ vitest.config.js
@@ -21,7 +21,8 @@ export default defineConfig({
 			svgs: resolve(__dirname, "src/assets/svgs/"),
 		},
 		mainFields: ["module"],
-		//File suffix name that needs to be omitted Note: If an ignored suffix name is configured here, an error will be reported if it is imported with a suffix name
+		//File suffix name that needs to be omitted
+		//Note: If an ignored suffix name is configured here, an error will be reported if it is imported with a suffix name
 		extensions: [".vue", ".js"]
 	},
 
