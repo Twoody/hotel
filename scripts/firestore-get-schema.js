6f424d5f3303fde9ec3a1afa33e2e56b1406721b
Tanner Woody <tanner.l.woody@gmail.com>
2025-04-01 20:40:40 -0700

    script support to get the schema from a firebase project

---
+++ scripts/firestore-get-schema.js
@@ -0,0 +1,22 @@
+	const { dbAdmin: dbAdmin, } = require("../src/firebaseAdmin")
+	db = dbAdmin
+
+async function extractSchema() {
+  const collections = await db.listCollections();
+  let schema = {};
+
+  for (const col of collections) {
+    schema[col.id] = {};
+    const snapshot = await col.limit(10).get(); // Sample docs to infer schema
+
+    snapshot.docs.forEach(doc => {
+      Object.entries(doc.data()).forEach(([field, value]) => {
+        schema[col.id][field] = typeof value;
+      });
+    });
+  }
+
+  console.log(JSON.stringify(schema, null, 2));
+}
+
+extractSchema();
