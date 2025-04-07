a38d04fae4d493b1358fa1cb8771b3775b7957ea
Tanner Woody <tanner.l.woody@gmail.com>
2025-04-06 23:37:57 -0700

    idk

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import {defineConfig} from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // Get current directory in ES module scope
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 
 // Load environment variables
 const host = process.env.VITE_HOST || "localhost";
