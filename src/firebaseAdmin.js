7fa697fbecdcd9791629ede87e28b87b72bb5409
Tanner Woody <tanner.l.woody@gmail.com>
2025-05-24 12:42:10 -0700

    lint + import method on json for lint

--- src/firebaseAdmin.js
+++ src/firebaseAdmin.js
@@ -1,26 +1,31 @@
 // src/firebaseAdmin.js
- import 'dotenv/config'; 
-//import * as admin from "firebase-admin";
-import admin from "firebase-admin";
-import serviceAccount from "../.keys/votel-f1c47-c69aa9c4722a.json" with { type: "json" };
+import "dotenv/config" 
+// import * as admin from "firebase-admin";
+import admin from "firebase-admin"
+import { createRequire } from "module"
 
-if (!admin.apps?.length) {
-	const databaseURL = process.env.VITE_FIREBASE_DATABASE_URL;
-	if (!databaseURL) {
-		console.error("ERROR: VITE_FIREBASE_DATABASE_URL is not defined in environment variables.");
-		process.exit(1); 
+const require = createRequire(import.meta.url)
+const serviceAccount = require("../.keys/votel-f1c47-c69aa9c4722a.json")
+
+if (!admin.apps?.length) 
+{
+	const databaseURL = process.env.VITE_FIREBASE_DATABASE_URL
+	if (!databaseURL) 
+	{
+		console.error("ERROR: VITE_FIREBASE_DATABASE_URL is not defined in environment variables.")
+		process.exit(1) 
 	}
 
 	admin.initializeApp({
 		credential: admin.credential.cert(serviceAccount),
 		databaseURL: databaseURL, // Use the variable
-	});
+	})
 }
 
-const dbAdmin = admin.firestore();
+const dbAdmin = admin.firestore()
 
 // Use ES Module export syntax instead of module.exports
 export {
 	admin,
-	dbAdmin,
-};
+	dbAdmin
+}
